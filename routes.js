1b53fd160b633b2a0501869fe5dc0a76a2a4fc8d
Gary Robinson <grobinson@virtualdev.com>
2023-08-15 19:45:53 -0400

    routes.js and package.json

---
+++ routes.js
@@ -0,0 +1,5 @@
+// This file was automatically added by edgio init.
+// You should commit this file to source control.
+import { Router } from '@edgio/core/router'
+
+export default new Router().static('public')
